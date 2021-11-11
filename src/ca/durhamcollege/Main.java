768889019eecc821c4e5edd9781d93058908ebd3
Devanshi <pdevanshi616@gmail.com>
2021-11-10 23:28:06 -0500

    Basic input and output test

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -1,8 +1,21 @@
+/*
+ * Author: Devanshi Patel
+ * Student ID: 100805084
+ * Date: November 10, 2021
+ *
+ */
 package ca.durhamcollege;
 
-public class Main {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String name;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        name = keyboard.nextLine();
+        System.out.printf("You Entered: %s", name);
     }
 }
