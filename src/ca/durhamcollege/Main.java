0997bf3ba38729a82d9f6d0bde94e0008728584c
Devanshi <pdevanshi616@gmail.com>
2021-11-10 23:42:44 -0500

    Input and Output test with two variables

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -2,8 +2,11 @@
  * Author: Devanshi Patel
  * Student ID: 100805084
  * Date: November 10, 2021
- *
+ * Course: OOP3200
+ * Assignment type: In-Class Activity 8
  */
+
+
 package ca.durhamcollege;
 
 import java.util.Scanner;
@@ -12,10 +15,21 @@ public class Main
 {
     public static void main(String[] args)
     {
-        String name;
+        // Define Scanner
         Scanner keyboard = new Scanner(System.in);
+
+        // Input
+        String name;
         System.out.print("Please enter your name: ");
         name = keyboard.nextLine();
-        System.out.printf("You Entered: %s", name);
+
+        float age = 0.0f;
+        System.out.print("Please enter your age: ");
+        age = keyboard.nextFloat();
+        System.out.println();
+
+        // Output
+        System.out.printf("You Entered: %s \n", name);
+        System.out.printf("You Entered: %.2f \n", age);
     }
 }
